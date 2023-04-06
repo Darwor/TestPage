0b3446321f1f5b1ec10ca461ec1721564049d194
Ruben Gonzalez <lukystar11099@gmail.com>
2023-04-05 21:17:59 -0600

    Hello Word

---
+++ form.js
@@ -0,0 +1,6 @@
+const usernameInput = document.getElementById("Username");
+const passwordInput = document.getElementById("Password");
+
+function enviar(evento) {
+    console.log(usernameInput.value);
+}
